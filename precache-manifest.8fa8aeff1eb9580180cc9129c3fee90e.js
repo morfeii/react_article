2fcc8fa892c6d57c72902a2b2799be0faadf500a
morfeii <morfeii88@gmail.com>
2020-02-12 00:21:14 +0200

    Updates

--- precache-manifest.8fa8aeff1eb9580180cc9129c3fee90e.js
+++ precache-manifest.8fa8aeff1eb9580180cc9129c3fee90e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb5b1aac7da35102f77d2d75bc7a1b99",
+    "revision": "c2cb68ad2d28cf16c1b6247cccbfd5ca",
     "url": "./index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.433df800.chunk.js.LICENSE"
   },
   {
-    "revision": "4538d0097f727c6411bb",
-    "url": "./static/js/main.6c719ce0.chunk.js"
+    "revision": "05ae5488db0a03a35c84",
+    "url": "./static/js/main.1bfacbe4.chunk.js"
   },
   {
     "revision": "62474d1959b237c5a49f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.8fa8aeff1eb9580180cc9129c3fee90e.js"
 );
 
 self.addEventListener('message', (event) => {
